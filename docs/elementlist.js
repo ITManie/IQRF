dd941b7044ccf1daf06a325d9ebb28307c1e0b19
Roman3349 <ondracek.roman@centrum.cz>
2016-04-30 16:25:56 +0200

    [Docs] Documentation moved to branch gh-pages

--- docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","BadFunctionCallException"],["c","Exception"],["c","InvalidArgumentException"],["c","IQRF\\Cloud\\Config"],["c","IQRF\\Cloud\\DI\\IQRFExtension"],["c","IQRF\\Cloud\\IQRF"],["c","IQRF\\Cloud\\Request\\Data"],["c","IQRF\\Cloud\\Request\\DataAPI"],["c","IQRF\\Cloud\\Request\\DataGW"],["c","IQRF\\Cloud\\Request\\Gateway"],["c","IQRF\\Cloud\\Response\\Data"],["c","IQRF\\Cloud\\Response\\DataAPI"],["c","IQRF\\Cloud\\Response\\DataGW"],["c","IQRF\\Cloud\\Response\\Status"],["c","LogicException"],["c","OutOfRangeException"]];
